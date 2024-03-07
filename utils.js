975c8c67d700944d7be684fda574bc7dd13d4d1d
Sargentogato <marlonjvelasquez@gmail.com>
2024-03-07 23:10:59 +0100

    utils.js delete code that was not necesary

--- utils.js
+++ utils.js
@@ -4,11 +4,6 @@ function letterW(char, size) {
   return textWidth(char);
 }
 
-// function mobileProp() {
-//   if (mobile) return 0.5;
-//   else return 1;
-// }
-
 function typeSize(n) {
   if (mobile) return 30;
   else return n;
@@ -17,29 +12,6 @@ function typeSize(n) {
 function padLeft(nr, n, str) {
   return Array(n - String(nr).length + 1).join(str || "0") + nr;
 }
-
-// function checkLyY() {
-//   return ly.maxY;
-// }
-
-// function resetFromStart() {
-//   opt = options["none"];
-//   ly.setSlide(0);
-// }
-
-// function endExperience() {
-//   opt = options["none"];
-//   ly.setSlide(5);
-// }
-
-// function goToPlayground() {
-//   opt = options["none"];
-//   ly.setSlide(4);
-// }
-
-// function backToPlay() {
-//   ly.setSlide(4);
-// }
 
 function setDarkMode(v = null) {
   if (v == null) {
